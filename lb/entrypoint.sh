4330e09fb387a4b1dc12dee569367cb290639e62
Filip <zgchuck@gmail.com>
2019-11-20 01:33:58 +0100

    Added keepalived

--- lb/entrypoint.sh
+++ lb/entrypoint.sh
@@ -3,5 +3,6 @@ set -e
 
 service rsyslog start
 haproxy -f /etc/haproxy/haproxy.cfg -db
+keepalived --dump-conf --log-console --log-detail --log-facility 7 --vrrp -f /etc/keepalived/keepalived.conf
 
-exec "$@"
+#exec "$@"

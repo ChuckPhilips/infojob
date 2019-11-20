e4d87619bb0f28a80ea259ea2a4a6e47bacda45b
Filip <zgchuck@gmail.com>
2019-11-20 13:48:42 +0100

    Updated entrypoint

--- lb/lb1/entrypoint.sh
+++ lb/lb1/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 set -e
 
-service rsyslog start
+#service rsyslog start
+/usr/sbin/rsyslogd
+/usr/sbin/keepalived --dump-conf --log-console --log-detail --log-facility 7 --vrrp -f /etc/keepalived/keepalived.conf
 #/usr/sbin/rsyslogd -n && \
-service keepalived start
-haproxy -f /etc/haproxy/haproxy.cfg -db
+#service keepalived start
+/usr/sbin/haproxy -f /etc/haproxy/haproxy.cfg -db
 #service haproxy start
-#keepalived --dump-conf --log-console --log-detail --log-facility 7 --vrrp -f /etc/keepalived/keepalived.conf
 
 #exec "$@"

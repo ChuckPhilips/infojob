34463623abffbd0b2f26d829a5b57c6461beb4e0
Filip <zgchuck@gmail.com>
2019-11-20 13:08:13 +0100

    Cleared excess files

--- lb/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-service rsyslog start
-haproxy -f /etc/haproxy/haproxy.cfg -db
-keepalived --dump-conf --log-console --log-detail --log-facility 7 --vrrp -f /etc/keepalived/keepalived.conf
-
-#exec "$@"

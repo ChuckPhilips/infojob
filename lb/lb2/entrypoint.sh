a2c3654f04a94fb90f2f629b01537f72e7f97e59
Filip <zgchuck@gmail.com>
2019-11-21 02:11:46 +0100

    Added ansible

--- lb/lb2/entrypoint.sh
+++ lb/lb2/entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 set -e
 
-#service rsyslog start
 /usr/sbin/rsyslogd
 /usr/sbin/keepalived --dump-conf --log-console --log-detail --log-facility 7 --vrrp -f /etc/keepalived/keepalived.conf
-#/usr/sbin/rsyslogd -n && \
-#service keepalived start
+/usr/sbin/sshd -D
 /usr/sbin/haproxy -f /etc/haproxy/haproxy.cfg -db
-#service haproxy start
 
-#exec "$@"
+exec "$@"

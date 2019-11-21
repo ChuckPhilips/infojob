cf555ee0ca6fae1ffa928ddd4e77e6d865ffdfe7
Filip <zgchuck@gmail.com>
2019-11-22 00:24:58 +0100

    Added bash to entrypoint so docker-compose tty can work and updated entrypoints services

--- lb/entrypoint.sh
+++ lb/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -e
 
-/usr/sbin/rsyslogd
-/usr/sbin/keepalived --dump-conf --log-console --log-detail --log-facility 7 --vrrp -f /etc/keepalived/keepalived.conf
-/usr/sbin/sshd
-/usr/sbin/haproxy -f /etc/haproxy/haproxy.cfg -db
+#/usr/sbin/rsyslogd
+#/usr/sbin/keepalived --dump-conf --log-console --log-detail --log-facility 7 --vrrp -f /etc/keepalived/keepalived.conf
+#/usr/sbin/sshd
+#/usr/sbin/haproxy -f /etc/haproxy/haproxy.cfg -db
+service rsyslog start
+service keepalived start
+service ssh start
+service haproxy start
 
 exec "$@"

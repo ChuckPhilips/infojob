a7cf34a1ad227ac0e3e3112ba6a7b1248c83c18d
Filip <zgchuck@gmail.com>
2019-11-21 10:41:28 +0100

    Updated docker-compose

--- lb/lb2/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-/usr/sbin/rsyslogd
-/usr/sbin/keepalived --dump-conf --log-console --log-detail --log-facility 7 --vrrp -f /etc/keepalived/keepalived.conf
-/usr/sbin/sshd
-/usr/sbin/haproxy -f /etc/haproxy/haproxy.cfg -db
-
-exec "$@"

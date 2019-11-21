2c05e01c398ae71d7d105adb7678c411b6a1f008
Filip <zgchuck@gmail.com>
2019-11-21 03:26:55 +0100

    Added ssh service to lb

--- lb/lb1/entrypoint.sh
+++ lb/lb1/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 /usr/sbin/rsyslogd
 /usr/sbin/keepalived --dump-conf --log-console --log-detail --log-facility 7 --vrrp -f /etc/keepalived/keepalived.conf
-/usr/sbin/sshd -D
+/usr/sbin/sshd
 /usr/sbin/haproxy -f /etc/haproxy/haproxy.cfg -db
 
 exec "$@"

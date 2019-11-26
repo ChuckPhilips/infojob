89dea1f3f26a0b96e4aa4a98c62717c6b1be172a
Filip <zgchuck@gmail.com>
2019-11-26 12:42:05 +0100

    Updated index.html

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 echo "<h1>Greeting from $HOSTNAME</h1>" > /usr/share/nginx/html/healthz.html
+echo "<h1>Greeting from $HOSTNAME</h1>" > /usr/share/nginx/html/index.html
 nginx -g 'daemon off;'
 
 exec "$@"

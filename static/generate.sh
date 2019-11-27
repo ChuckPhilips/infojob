e6fa5d362a7f43241a7802d8d26f8f91fbdc3aa6
Filip <zgchuck@gmail.com>
2019-11-27 13:07:46 +0100

    Fixed typo

--- static/generate.sh
+++ static/generate.sh
@@ -2,6 +2,6 @@
 
 for domain in infojob.com www.infojob.com infojob2.com www.infojob2.com; do
 	openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=HR/ST=Zagrebacka/L=Zagreb/O=Infojob/CN=${domain}" -keyout "${domain}.key"  -out "${domain}.crt"
-	cat "${domain}.crt" "${domain}.key" >> /var/www/html/"${domain}.pem"
+	cat "${domain}.crt" "${domain}.key" > /var/www/html/"${domain}.pem"
 	rm "${domain}.key" "${domain}.crt"
 done

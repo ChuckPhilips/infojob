357075484c5713b66abb71288987bcc8208bf7d3
Filip <zgchuck@gmail.com>
2019-11-27 13:44:05 +0100

    Updated generate.sh

--- static/generate.sh
+++ static/generate.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 for domain in infojob.com www.infojob.com infojob2.com www.infojob2.com; do
-	openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=HR/ST=Zagrebacka/L=Zagreb/O=Infojob/CN=${domain}" -keyout "${domain}.key"  -out "${domain}.crt"
-	cat "${domain}.crt" "${domain}.key" > /var/www/html/"${domain}.pem"
-	rm "${domain}.key" "${domain}.crt"
+	echo "Generating new cert for ${domain}."
+	openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=HR/ST=Zagrebacka/L=Zagreb/O=Infojob/CN=${domain}" -keyout "/tmp/${domain}.key"  -out "/tmp/${domain}.crt" 2>/dev/null
+	cat "/tmp/${domain}.crt" "/tmp/${domain}.key" > /var/www/html/"${domain}.pem"
+	rm "/tmp/${domain}.key" "/tmp/${domain}.crt"
 done
+
+echo 'Changes will be active in about 1 minute.'

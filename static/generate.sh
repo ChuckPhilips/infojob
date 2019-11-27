1d67c13e20d8c824334bb22fb9e526be2c4a88f0
Filip <zgchuck@gmail.com>
2019-11-27 13:00:48 +0100

    Updated generate.sh script

--- static/generate.sh
+++ static/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for domain in infojob.com infojob2.com infojob2.com www.infojob2.com; do
+for domain in infojob.com www.infojob.com infojob2.com www.infojob2.com; do
 	openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=HR/ST=Zagrebacka/L=Zagreb/O=Infojob/CN=${domain}" -keyout "${domain}.key"  -out "${domain}.crt"
 	cat "${domain}.crt" "${domain}.key" >> /var/www/html/"${domain}.pem"
 	rm "${domain}.key" "${domain}.crt"
